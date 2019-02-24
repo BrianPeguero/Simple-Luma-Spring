62c1fdbae92ae59146241b0ea52d03da87d74db3
Brian <brianpeguero@gmail.com>
2019-02-24 17:40:50 -0500

    created the product model

--- src/models/Product.java
+++ src/models/Product.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Product {
-
+	
+	private int id;
+	private String sku;
+	private String name;
+	private int attribute_set;
+	private double price;
+	private String product_type;
+	private int[] category_id;
+	private String image_file;
+	private String description;
+	private int[] configurable_product_links;
+	
 }
