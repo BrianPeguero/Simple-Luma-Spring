f996ac4fe361ae2d0bc0dd114ce863ffede56b43
Brian <brianpeguero@gmail.com>
2019-02-26 13:22:32 -0600

    finished creating the product model

--- src/models/Product.java
+++ src/models/Product.java
@@ -13,4 +13,81 @@ public class Product {
 	private String description;
 	private int[] configurable_product_links;
 	
+	
+	public Product(int id, String sku, String name, int attribute_set, double price, String product_type,
+			int[] category_id, String image_file, String description, int[] configurable_product_links) {
+		this.id = id;
+		this.sku = sku;
+		this.name = name;
+		this.attribute_set = attribute_set;
+		this.price = price;
+		this.product_type = product_type;
+		this.category_id = category_id;
+		this.image_file = image_file;
+		this.description = description;
+		this.configurable_product_links = configurable_product_links;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAttribute_set() {
+		return attribute_set;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public String getProduct_type() {
+		return product_type;
+	}
+	public int[] getCategory_id() {
+		return category_id;
+	}
+	public String getImage_file() {
+		return image_file;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public int[] getConfigurable_product_links() {
+		return configurable_product_links;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAttribute_set(int attribute_set) {
+		this.attribute_set = attribute_set;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	public void setCategory_id(int[] category_id) {
+		this.category_id = category_id;
+	}
+	public void setImage_file(String image_file) {
+		this.image_file = image_file;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setConfigurable_product_links(int[] configurable_product_links) {
+		this.configurable_product_links = configurable_product_links;
+	}
+	
 }
