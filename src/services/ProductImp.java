37e9faf453dac769cc4e429ecf3ea5618c3b8fd2
Brian <brianpeguero@gmail.com>
2019-03-06 22:12:40 -0500

    created the implementation for the method that get the sku and product stored into a hash map

--- src/services/ProductImp.java
+++ src/services/ProductImp.java
@@ -158,8 +158,59 @@ public class ProductImp implements ProductInt {
 
 	@Override
 	public LinkedHashMap<String, Product> getAllSKUAndProduct() {
-		// TODO Auto-generated method stub
-		return null;
+		String token = "xq0o8r3bkuvlf66xv5pmpwp8jax9vvvv";
+		String url = "http://localhost/magento2/rest/V1/products/?searchCriteria=";
+		
+		//create the client to execute the request
+		HttpClient client = HttpClientBuilder.create().build();
+		
+		//creates the request with HTTP request method for the target url
+		HttpGet request = new HttpGet(url);
+		
+		//add to the head of the request using the token and content type you want back
+		request.addHeader("Authorization", "Bearer " + token);
+		request.addHeader("Accept", "application/xml");
+		request.addHeader("Content-Type", "application/xml");
+		
+		LinkedHashMap<String, Product> lhm = null;
+		try {
+			
+			HttpResponse response = client.execute(request);
+			
+			//xml DOM document parser 
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	        
+	        Document doc = dBuilder.parse(response.getEntity().getContent());
+	        
+	        NodeList skuList = doc.getElementsByTagName("sku");
+	        
+	        lhm = new LinkedHashMap<String, Product>();
+	        
+	        for(int i = 0; i < skuList.getLength(); i++) {
+	        	String sku = skuList.item(i).getTextContent();
+	        	Product product = getProductBySKU(sku);
+	        	lhm.put(sku, product);
+	        }
+	        
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return lhm;
 	}
 
 }
